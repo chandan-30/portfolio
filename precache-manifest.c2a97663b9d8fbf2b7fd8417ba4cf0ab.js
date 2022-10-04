c49e8331f20d6b7d452eddad8d49eac03b086d5c
chandan-30 <saichandhan.yata44@gmail.com>
2022-10-04 13:33:02 +0530

    Updates

---
+++ precache-manifest.c2a97663b9d8fbf2b7fd8417ba4cf0ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4376d3fc4a4ab5ffe6df",
+    "url": "/github.io/portfolio/static/css/main.f42f0dc5.chunk.css"
+  },
+  {
+    "revision": "4376d3fc4a4ab5ffe6df",
+    "url": "/github.io/portfolio/static/js/main.4376d3fc.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/github.io/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "37628a64f39afa494ec4",
+    "url": "/github.io/portfolio/static/js/runtime~main.37628a64.js"
+  },
+  {
+    "revision": "ffae56c55db811dac95ed98da55735c4",
+    "url": "/github.io/portfolio/index.html"
+  }
+];
